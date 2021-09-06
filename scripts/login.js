da440cb5251d28496374de29fa0536daa9977a94
osdev <65999136+OSFounder@users.noreply.github.com>
2021-09-06 13:44:06 +1200

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -71,13 +71,13 @@ function authStateListener() {
             console.error(e);
          }
          if (firebase.auth().currentUser.emailVerified) {
-              userUrl = new URL('https://nexuslive.tech/userPage.html');
+              userUrl = new URL('https://404soundandlighting/profile');
               userUrl.searchParams.append('UID', uid);
               userUrl.searchParams.append('accountVerified', 'true');
               window.location.href = userUrl;
          }
          else {
-             userUrl = new URL('https://nexuslive.tech/userPage.html');
+             userUrl = new URL('https://404soundandlighting/profile');
              userUrl.searchParams.append('accountVerified', 'false');
              window.location.href = userUrl;
              sendVerificationEmail();

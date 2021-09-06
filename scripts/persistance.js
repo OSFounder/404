6ffa3329e4e535fa9ba83c7610b004b152426b58
osdev <65999136+OSFounder@users.noreply.github.com>
2021-09-06 15:28:08 +1200

    Update persistance.js

--- scripts/persistance.js
+++ scripts/persistance.js
@@ -2,6 +2,23 @@ function setPersistenceSession() {
    var email = txtEmail;
    var password = txtPword;
 
+    firebase.auth().setPersistenceHigh(firebase.auth.Auth.Persistence.LOCAL)
+    .then(() => {
+      // Existing and future Auth states are now persisted in the current
+      // session only. Closing the window would clear any existing state even
+      // if a user forgets to sign out.
+      // ...
+      // New sign-in will be persisted with session persistence.
+      return firebase.auth().signInWithEmailAndPassword(email, password);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  // [END auth_set_persistence_session]
+}
+
   // [START auth_set_persistence_session]
   firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
     .then(() => {

64e6f38a99a130d5af80464a393e21683ad1f890
osdev <65999136+OSFounder@users.noreply.github.com>
2021-09-08 16:04:13 +1200

    Update overhaul.js

--- scripts/overhaul.js
+++ scripts/overhaul.js
@@ -2,12 +2,14 @@ const content = document.getElementById("content");
 const warn = document.getElementById("warning-modal");
 const warn_submit = document.getElementById("warn-submit");
 const demo = document.getElementById("demo-video");
+const mobile_demo = document.getElementById('demo-video-mobile");
 
 warn.classList.remove("hidden-element");
 
 warn_submit.addEventListener("click", e=> {
-  demo.classList.add("hidden-element");
-  warn.classList.add("hidden-element");
   document.body.style.background = "#FFFFFF"
+  demo.classList.add("hidden-element");
+  mobile_demo.classList.add("hidden-element");
+  warn.classList.add("hidden-element");
   content.classList.remove("hidden-element");
 });

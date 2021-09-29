262888d52b83ac67a80c444f7d3017242f294e5f
osdev <65999136+OSFounder@users.noreply.github.com>
2021-09-30 12:07:06 +1300

    Update overhaul.js

--- scripts/overhaul.js
+++ scripts/overhaul.js
@@ -105,7 +105,7 @@ $(document).ready(function () {
 $(window).on("load",function() {
   $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $("#mid-description-one").each(function() {
+    $(".fade").each(function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
  
       if (objectBottom < windowBottom) { 
